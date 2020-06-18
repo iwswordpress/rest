5aa051cc32d164f79979ecb2171aaca1f33ad195
Craig West <iwswordpress@gmail.com>
2020-06-18 09:17:50 +0100

    edit

--- code/php/page-ajax.php
+++ code/php/page-ajax.php
@@ -5,8 +5,9 @@ get_header(); ?>
 <!-- Page Container -->
 <?php include 'server.php'; ?>
 <main>
-   
-<div class="w3-container w3-content" style="max-width:1200px;margin-top:10px;border:2px solid #2196f3;border-radius:10px;">
+
+    <div class="w3-container w3-content"
+        style="max-width:1200px;margin-top:10px;border:2px solid #2196f3;border-radius:10px;">
         <!-- W3 CSS Grid -->
         <div class="w3-row">
             <div class="w3-col m12">
@@ -18,12 +19,12 @@ get_header(); ?>
                     </script>
                     <!-- <h1 style="font-size:40px;">WordCamp Dublin</h1> -->
                 </header>
-              
+
                 <div class="w3-container" style="width:100%;">
                     <div class="w3-row">
                         <div class="w3-col m12" style="margin-top:20px;padding-left:30px;padding-right:30px;">
                             <div style="margin-bottom:30px;">
-                            <input id="btnPosts" name="btnPosts" class="w3-btn w3-border w3-large w3-blue"
+                                <input id="btnPosts" name="btnPosts" class="w3-btn w3-border w3-large w3-blue"
                                     value="LOAD POSTS">
                                 <input id="btnSearch" name="btnSearch" class="w3-btn w3-border w3-large w3-blue"
                                     value="LOAD MySQL">
@@ -51,17 +52,20 @@ get_header(); ?>
                                     console.log(url);
                                     fetch(url)
                                         .then(response => {
-                                                console.log(response);
-                                                return response.json();
-                                            })
+                                            console.log(response);
+                                            return response.json();
+                                        })
                                         .then(data => {
                                             // Prints result from `response.json()` in get Request
                                             console.log("DATA", data)
                                             console.log(data.length);
-                                            let outputData = '<?php echo $SITE; ?>' + 'wp-json/wordcamp/v2/districts';
-                                            outputData += '<table class="w3-table w3-border w3-striped" ><tr><th>ID</th><th>CITY</th></tr>';
+                                            let outputData = '<?php echo $SITE; ?>' +
+                                                'wp-json/wordcamp/v2/districts';
+                                            outputData +=
+                                                '<table class="w3-table w3-border w3-striped" ><tr><th>ID</th><th>CITY</th></tr>';
                                             for (var i = 0; i < data.length; i++) {
-                                                outputData += "<tr><td> " + data[i].ID + "</td><td>" + data[i].Name + "</td></tr>";
+                                                outputData += "<tr><td> " + data[i].ID + "</td><td>" + data[i]
+                                                    .Name + "</td></tr>";
                                             }
                                             outputData += "</table>";
                                             const main = document.getElementById('mainContent');
@@ -71,46 +75,48 @@ get_header(); ?>
                                         .catch(error => console.error(error))
                                 }
                                 // GET POSTS
-                                function loadPosts() {
+                                function loadPosts) {
                                     // Generate URL
-                                    const url =  '<?php echo $SITE; ?>' + 'wp-json/wp/v2/posts';
+                                    const url = '<?php echo $SITE; ?>' + 'wp-json/wp/v2/posts';
                                     console.log(url);
                                     fetch(url)
                                         .then(response => {
-                                                console.log("We get back a STREAM initially...");
-                                                console.log(response);
-                                                return response.json();
-                                            })
+                                            console.log("We get back a STREAM initially...");
+                                            console.log(response);
+                                            return response.json();
+                                        })
                                         .then(dataArray => {
                                             // Prints result from `response.json()` in get Request
                                             console.log("RESPONSE.JSON() is used to convert stream to JSON...");
                                             console.log("POSTS", dataArray)
                                             console.log(dataArray.length);
                                             console.log("Number of posts is " + dataArray.length);
-                                            let outputData =  '<?php echo $SITE; ?>' + 'wp-json/wp/v2/posts';
-                                          
-                                            outputData += '<table class="w3-table w3-border w3-striped "><tr><th>ID</th><th>TITLE</th></tr>';
+                                            let outputData = '<?php echo $SITE; ?>' + 'wp-json/wp/v2/posts';
+
+                                            outputData +=
+                                                '<table class="w3-table w3-border w3-striped "><tr><th>ID</th><th>TITLE</th></tr>';
                                             for (let i = 0; i < dataArray.length; i++) {
-                                                outputData += "<tr><td>" + dataArray[i].id + "</td><td>" + dataArray[i].title.rendered + "</td></tr>";
+                                                outputData += "<tr><td>" + dataArray[i].id + "</td><td>" +
+                                                    dataArray[i].title.rendered + "</td></tr>";
                                             }
                                             outputData += "</table>";
-                                            
+
                                             const main = document.getElementById('mainContent');
                                             main.innerHTML = outputData;
-                                              
+
                                         })
                                         .catch(error => console.error(error))
                                 }
                             </script>
                             <!-- ================ MAIN CODE ======================= -->
                             <div>
-                            <br>
+                                <br>
 
-                        </div><!-- endcard -->
-                        <br><br><br>
-                    </div><!-- end col page -->
-                </div><!-- end page row -->
-            </div><!-- end page container -->
+                            </div><!-- endcard -->
+                            <br><br><br>
+                        </div><!-- end col page -->
+                    </div><!-- end page row -->
+                </div><!-- end page container -->
 </main>
 <!-- End Page Container -->
 <br><br><br>
